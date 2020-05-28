dad81b183e649bb717b933de37f1d5c5a4625c29
hoaithu3t <hoaithu3t@gmail.com>
2020-05-28 09:22:52 +0700

    commit

--- api/setCard.js
+++ api/setCard.js
@@ -1,4 +1,5 @@
 const authMdw = require("../middleware/auth");
+<<<<<<< HEAD
 const { createSetCard , getListSetCard, getSetCard} = require("../services/setCard");
 
 const router = require("express").Router();
@@ -16,5 +17,42 @@ router.get("/", authMdw({ optional: true }), (req, res) => {
 router.get("/:slug", (req, res) => {
   const { slug } = req.params;
   getSetCard(slug).then((setCard) => res.json(setCard));
+=======
+const {
+  createSetCard,
+  getSetCard,
+  updateSetCard,
+  deleteSetCard,
+} = require("../services/setCard");
+
+const router = require("express").Router();
+
+router.post("/", authMdw(), (req, res) => {
+  createSetCard(req.user, req.body).then((post) => res.json(post));
+});
+
+// router.get("/", authMdw({ optional: true }), (req, res) => {
+//   getListPost(req.user).then((posts) => {
+//     res.json(posts);
+//   });
+// });
+
+router.get("/:slug", (req, res) => {
+  const { slug } = req.params;
+  getSetCard(slug).then((post) => res.json(post));
+});
+
+router.put("/", authMdw({ optional: true }), (req, res) => {
+  console.log(res.body);
+  updateSetCard(req.body._id, req.body).then((newSetCard) => {
+    res.json(newSetCard);
+  });
+});
+router.delete("/", (req, res) => {
+  const id = req.body;
+  deleteSetCard(id)
+    .then((setCard) => res.json({ success: true }))
+    .catch((err) => res.json({ success: false, err: err.message }));
+>>>>>>> fd0d4a96f07bd2ff793b7b0c20c89a85083b50af
 });
 module.exports = router;
--- services/setCard.js
+++ services/setCard.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 const { SetCard } = require("../models/setCard");
+const ERROR = require("../types/error");
+
 
 const createSetCard = (setCard) => {
   const user = {
     _id: "5ecd37b61e1e982ddcfa95c5",
     username: "dvtam99",
   };
+
   const newSetCard = new SetCard({
     title: setCard.title,
     avatar: setCard.avatar,
@@ -58,29 +61,53 @@ const getListSetCard = async (currentUser) => {
     .exec();
   return setCards;
 };
+
+
 const getSetCard = async (slug) => {
-  const SetCards = await SetCard.aggregate()
+  const setCard = await SetCard.aggregate()
     .match({ slug: slug })
     .limit(1)
     .lookup({
-      from: 'users',
-      localField: 'author',
-      foreignField: '_id',
-      as: 'author',
+      from: "users",
+      localField: "author",
+      foreignField: "_id",
+      as: "author",
     })
-    .unwind('author')
+    .unwind("author")
     .project({
       title: 1,
-      photoUrl: 1,
-      contentFilePath: 1,
-      tags: 1,
-      slug: 1,
-      likeCount: { $size: '$like' },
-      createdAt: 1,
-      updatedAt: 1,
+      avatar: 1,
+      detail: 1,
+      finish: 1,
+      date_create: 1,
       author: 1,
+      contentFilePath: 1,
+      slug: 1,
     })
     .exec();
-  return setCards[0];
+  return setCard[0];
 };
-module.exports = { createSetCard, getListSetCard, getSetCard };
+
+const updateSetCard = async (id, setCard) => {
+  const newSetCard = await SetCard.findOneAndUpdate({ _id: id }, setCard, {
+    new: true,
+  });
+  return newSetCard;
+};
+const deleteSetCard = async (id) => {
+  const newSetCard = await SetCard.findOne({
+    _id: id,
+  });
+  if (!newSetCard) throw new Error(ERROR.SET_CARD_NOT_EXISTED);
+  await SetCard.deleteOne({ _id: id });
+  return true;
+};
+
+module.exports = {
+  createSetCard,
+  getListSetCard,
+  getSetCard,
+  deleteSetCard,
+  updateSetCard,
+};
+
