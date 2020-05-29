d9a075335858672d451e2ab7036f8f48897d5637
hoaithu3t <hoaithu3t@gmail.com>
2020-05-30 02:48:04 +0700

    modify model user

--- api/setCard.js
+++ api/setCard.js
@@ -1,11 +1,16 @@
 const authMdw = require("../middleware/auth");
-<<<<<<< HEAD
-const { createSetCard , getListSetCard, getSetCard} = require("../services/setCard");
+const {
+  createSetCard,
+  getSetCard,
+  updateSetCard,
+  deleteSetCard,
+  getListSetCard
+} = require("../services/setCard");
 
 const router = require("express").Router();
 
-router.post("/", authMdw({ optional: true }), (req, res) => {
-  createSetCard(req.body).then((setCard) => res.json(setCard));
+router.post("/", authMdw(), (req, res) => {
+  createSetCard(req.user, req.body).then((post) => res.json(post));
 });
 
 router.get("/", authMdw({ optional: true }), (req, res) => {
@@ -17,29 +22,6 @@ router.get("/", authMdw({ optional: true }), (req, res) => {
 router.get("/:slug", (req, res) => {
   const { slug } = req.params;
   getSetCard(slug).then((setCard) => res.json(setCard));
-=======
-const {
-  createSetCard,
-  getSetCard,
-  updateSetCard,
-  deleteSetCard,
-} = require("../services/setCard");
-
-const router = require("express").Router();
-
-router.post("/", authMdw(), (req, res) => {
-  createSetCard(req.user, req.body).then((post) => res.json(post));
-});
-
-// router.get("/", authMdw({ optional: true }), (req, res) => {
-//   getListPost(req.user).then((posts) => {
-//     res.json(posts);
-//   });
-// });
-
-router.get("/:slug", (req, res) => {
-  const { slug } = req.params;
-  getSetCard(slug).then((post) => res.json(post));
 });
 
 router.put("/", authMdw({ optional: true }), (req, res) => {
@@ -53,6 +35,5 @@ router.delete("/", (req, res) => {
   deleteSetCard(id)
     .then((setCard) => res.json({ success: true }))
     .catch((err) => res.json({ success: false, err: err.message }));
->>>>>>> fd0d4a96f07bd2ff793b7b0c20c89a85083b50af
 });
 module.exports = router;
